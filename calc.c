29a74baae02cf97f1c4fec60d9aa297aaea21ca7
netsanet <netsanetbikes@gmail.com>
2023-02-03 04:13:23 -0800

    calculator using function

--- calc.c
+++ calc.c
@@ -1,32 +1,72 @@
 #include <stdio.h>
+int add(int a, int b);
+int mul(int a, int b);
+int div(int a, int b);
+int mod(int a, int b);
+int dif(int a, int b);
 int main(void)
 {
 	int a;
 	int b;
 	char o;
-	printf("Enter a and b");
-	scanf("%d%d", &a);
-	printf("Enter the operation");
-	scanf("%c", &o);
+	printf("Enter +, /, -, *, %");
+        scanf("%c", &o);
+	printf("Enter the first number: ");
+	scanf("%d", &a);
+	printf("Enter the second number: ");
+        scanf("%d", &b);
 	switch(o) {
 		case '+':
-			printf("a + b = %d", a+b);
+			printf("a + b = %d\n", add(a,b));
 			break;
 		case '-':
-			printf("a - b = %d", a-b);
+			printf("a - b = %d\n", dif(a,b));
                         break;
 		case '*':
-			printf("a * b = %d", a*b);
+			printf("a * b = %d\n", mul(a,b));
                         break;
 		case '/':
-			printf("a / b = %d", a/b);
+			if (b!=0){
+			printf("a / b = %d\n", div(a,b));
+			}
+			else 
+			{
+			printf("the second number must be different from Zero\n");}
 			break;
 		case '%':
-			printf("a / b = %d", a%b);
+			if (b!=0){
+			printf("a / b = %d\n", mod(a,b));
+			}
+                        else                                              {
+                        printf("the second number must be different from Zero\n");}
 			break;
 		default:
-			printf("
+			printf("Invalid operator used\n");
 	}
+	return (0);
+}
+int add(int a, int b)
+{
+return (a+b);
+}
 
+int mul(int a, int b)
+{
+return (a*b);
+}
+int dif(int a, int b)
+{
+return (a-b);
+}
+int div(int a, int b)
+{
+	if (b!=0)
+	{
+		return (a/b);
+	}
+}
+int mod(int a, int b)
+{
+return (a%b);
 }
 
